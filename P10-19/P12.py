875f0076b5b526ef46ac6bc3df8bbabd967c155b
Hanthebot <56969399+Hanthebot@users.noreply.github.com>
2022-07-24 23:47:11 +0900

    P12 - Case resolved
    Case resolved
    Grammar improved
    Algorithm improved

--- P10-19/P12.py
+++ P10-19/P12.py
@@ -1,21 +1,42 @@
-def getfac(n):
-    fac=[]
-    for a in range(1,n+1):
-        if n%a==0:
-            fac.append(a)
-    return fac
+import time
+import math
 
-def tri(n):
-    return int((n*n+n)/2)
+#revised on 2022/07/24 22:53
 
-def main(h):
-    d=[]
-    z=1
-    while len(getfac(tri(z)))<h:
-        z+=1
-        d.append(len(getfac(tri(z))))
-        print(max(d), end="\r")
-    return tri(z)
+def fo2(a):
+    return "{0:.2f}".format(a)
+
+def inTime(t):
+    return f"{int(t / 3600)}:{int(t % 3600 / 60)}:{fo2(t % 60)}s"
+
+def evenDivide(n):
+    if n % 2:
+        return int(n / 2)
+    else:
+        return int((n - 1) / 2)
+
+def getFactors(n):
+    fac = [num for num in range(1, int(math.sqrt(n)) + 1) if n % num == 0]
+    if fac[-1] == math.sqrt(n):
+        return 2 * len(fac) -1
+    else:
+        return 2 * len(fac)
+
+def triangleNumber(n):
+    return int((n * (n + 1)) / 2)
+
+def main(target, t = time.time()):
+    divisors = [0]
+    i = 0
+    number = 0
+    while max(divisors) < target:
+        i += 1
+        number += i
+        divisors.append(getFactors(number))
+        print(f"i: {i}, Target: {target}, Latest: {max(divisors)}, Consumed time: {inTime(time.time() - t)}     ", end="\r")
+    return number
 
 if __name__=="__main__":
-    print(main(500))
+    time_i = time.time()
+    print("\n", main(500, time_i))
+    print(f"Done, took: {inTime(time.time() - time_i)}      ")
