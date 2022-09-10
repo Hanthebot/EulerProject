8d73273529db813805da259444893e4b43aacd65
Hanthebot <56969399+Hanthebot@users.noreply.github.com>
2022-09-10 19:17:41 +0700

    Update P15.py

--- P10-19/P15.py
+++ P10-19/P15.py
@@ -1,11 +1,21 @@
-def main(n):
-    if n==1:
+import time
+
+#revised on 2022/07/24 22:53
+
+def fo2(a):
+    return "{0:.2f}".format(a)
+
+def inTime(t):
+    return f"{int(t / 3600)}:{int(t % 3600 / 60)}:{fo2(t % 60)}s"
+
+def main(n, t = time.time()):
+    print(f"n: {n}, Consumed time: {inTime(time.time() - t)}", end = "\r")
+    if n == 1:
         return 2
     else:
-        return main(n-1)*2+2
-
-for r in range(1, 21):
-    print(main(r))
+        return main(n - 1) * 2 + 2
 
 if __name__=="__main__":
-    print(main(20))
+    time_i = time.time()
+    print("\n", main(20))
+    print(f"Done, took: {inTime(time.time() - time_i)}")
